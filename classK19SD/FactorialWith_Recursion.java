5024c5bc05113f2ac0501a553ceed575ae9849e0
srv-shivam <shivamsrivastava222001@gmail.com>
2021-09-24 00:24:07 +0530

    Code of FactorialWith_Recursion.

--- classK19SD/FactorialWith_Recursion.java
+++ classK19SD/FactorialWith_Recursion.java
@@ -3,4 +3,21 @@ import java.util.Scanner;
 
 public class FactorialWith_Recursion {
 
+    // Factorial of number using recursion
+    private static void factorialOfNumber(int n, int factorial) {
+        if (n==1) {
+            System.out.println("Factorial is : " + factorial);
+            return;
+        }
+        factorial *= n;
+        factorialOfNumber(n-1, factorial);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        factorialOfNumber(n, 1);
+    }
 }
