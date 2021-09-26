ece724332c2b38327cd6bbcc5040bc6011bc0bde
srv-shivam <shivamsrivastava222001@gmail.com>
2021-09-27 02:29:30 +0530

    Code of to remove duplicate characters in String.

--- Recursion/RemoveDuplicates.java
+++ Recursion/RemoveDuplicates.java
@@ -1,4 +1,28 @@
 package Recursion;
 
 public class RemoveDuplicates {
+
+    private static boolean charArray[] = new boolean[26];
+
+    private static void removeDuplicate(String str, int idx, String newString) {
+
+        if (idx == str.length()) {
+            System.out.print(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        int index = currChar - 'a';
+
+        if (!charArray[index]) {
+            charArray[index] = true;
+            newString += currChar;
+        }
+        removeDuplicate(str, idx + 1, newString);
+    }
+
+    public static void main(String[] args) {
+        String str = "abbccdad";
+        removeDuplicate(str, 0, "");
+    }
+
 }
