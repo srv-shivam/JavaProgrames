72a2546fc7eef88730cbff8243138db9d3694054
srv-shivam <shivamsrivastava222001@gmail.com>
2021-09-27 03:14:14 +0530

    Code to unique PrintSubsequences of given String.

---
+++ Recursion/UniqueSubSequences.java
@@ -0,0 +1,28 @@
+package Recursion;
+import java.util.HashSet;
+
+public class UniqueSubSequences {
+
+    private static void subsequences(String str, int idx, String newString, HashSet<String> set) {
+
+        if (idx == str.length()) {
+            if (!set.contains(newString)) {
+                System.out.println(newString);
+                set.add(newString);
+            }
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        subsequences(str, idx + 1, newString + currChar, set);
+
+        subsequences(str, idx + 1, newString, set);
+
+    }
+
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        subsequences("aaa", 0, "", set);
+    }
+
+}
