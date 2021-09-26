5d59d1742da036b739daeef90bac2c704f505d46
srv-shivam <shivamsrivastava222001@gmail.com>
2021-09-27 02:11:09 +0530

    Code of to move particular character to the end of the string.

---
+++ Recursion/MoveCharacter.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+public class MoveCharacter {
+
+    private static void moveAllX(String str, int idx, int count, String newString) {
+
+        if (idx == str.length()) {
+            for (int i=0; i<count; ++i) {
+                newString += 'x';
+            }
+            System.out.print(newString );
+            return;
+        }
+
+        if (str.charAt(idx) == 'x') {
+            ++count;
+        } else {
+            newString += str.charAt(idx);
+        }
+        moveAllX(str, idx + 1, count, newString);
+    }
+
+    public static void main(String[] args) {
+        String str = "axbxxcxd";
+        moveAllX(str, 0, 0, "");
+    }
+
+}
