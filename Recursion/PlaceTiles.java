dfc94b12deb3c835ccb4efa62eb62f35a53b8685
srv-shivam <shivamsrivastava222001@gmail.com>
2021-09-30 00:22:33 +0530

    Code to PlaceTiles.

---
+++ Recursion/PlaceTiles.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+public class PlaceTiles {
+
+    private static int countPlacements(int n, int m) {
+
+        if (n == m) {
+            return 2;
+        }
+        if (n < m) {
+            return 1;
+        }
+
+        // vertically placement
+        int verPlacements = countPlacements(n - m, m);
+
+        // horizontally placement
+        int horPlacements = countPlacements(n - 1, m);
+
+        return verPlacements + horPlacements;
+    }
+
+    public static void main(String[] args) {
+        int n = 4, m = 2;
+        System.out.println(countPlacements(n, m));
+    }
+}
