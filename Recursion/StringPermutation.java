6fd18d30ddcc7c8a274b5df2f3665d17a691650c
srv-shivam <shivamsrivastava222001@gmail.com>
2021-09-29 21:32:00 +0530

    Code to generate permutation of given string.

---
+++ Recursion/StringPermutation.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class StringPermutation {
+
+    private static void printPermutation(String str, String permutation) {
+
+        if (str.length() == 0) {
+            System.out.println(permutation);
+            return;
+        }
+
+        for (int i=0; i<str.length(); i++) {
+            char currChar = str.charAt(i);
+            String newString = str.substring(0, i) + str.substring(i+1);
+            printPermutation(newString, permutation + currChar);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        printPermutation(str, "");
+    }
+}
