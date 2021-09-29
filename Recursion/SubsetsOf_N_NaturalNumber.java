c841538e60f1e9bdb63682da7feeb0856962e00f
srv-shivam <shivamsrivastava222001@gmail.com>
2021-09-30 00:52:56 +0530

    Code to print/generate subsets of N natural numbers.

---
+++ Recursion/SubsetsOf_N_NaturalNumber.java
@@ -0,0 +1,37 @@
+package Recursion;
+
+public class SubsetsOf_N_NaturalNumber {
+
+    // Ways1 to print all subsets of n natural numbers
+    private static void printSubsets(int start, int n, String subsets) {
+
+        if (start > n) {
+            System.out.println(subsets);
+            return;
+        }
+
+        //to be
+        printSubsets(start + 1, n, subsets + start);
+
+        //not to be
+        printSubsets(start + 1, n, subsets);
+    }
+
+    //Ways2 to print all subsets of n natural numbers
+    private static void subsetsComb(int n, String subsets) {
+
+        if (n == 0) {
+            System.out.println(subsets);
+            return;
+        }
+
+        subsetsComb(n - 1, subsets + n);
+
+        subsetsComb(n - 1, subsets);
+    }
+
+    public static void main(String[] args) {
+//        printSubsets(1, 3, "");
+        subsetsComb(3, "");
+    }
+}
