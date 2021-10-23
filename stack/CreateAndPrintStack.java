5cd2b016626a470f5f77a5a2a7ec2548eb37976c
srv-shivam <shivamsrivastava222001@gmail.com>
2021-10-24 00:48:11 +0530

    Implementation of Custom Stack class.

--- stack/CreateAndPrintStack.java
+++ stack/CreateAndPrintStack.java
@@ -1,9 +1,41 @@
 package stack;
-import java.util.Scanner;
 
 public class CreateAndPrintStack {
 
     public static void main(String[] args) {
+
+        Stack stack = new Stack();
+
+        stack.push(10);
+        stack.push(15);
+        stack.push(20);
+        stack.push(25);
+        stack.push(30);
+
+        System.out.println("Top = " + stack.peek());
+        System.out.println("Popped = " + stack.pop());
+        System.out.println("New Top = " + stack.peek());
+
+        System.out.println("\nTop = " + stack.peek());
+        System.out.println("Popped = " + stack.pop());
+        System.out.println("New Top = " + stack.peek());
+
+        System.out.println("\nTop = " + stack.peek());
+        System.out.println("Popped = " + stack.pop());
+        System.out.println("New Top = " + stack.peek());
+
+        System.out.println("\nTop = " + stack.peek());
+        System.out.println("Popped = " + stack.pop());
+        System.out.println("New Top = " + stack.peek());
+
+        System.out.println("\nTop = " + stack.peek());
+        System.out.println("Popped = " + stack.pop());
+        System.out.println("New Top = " + stack.peek());
+
+        System.out.println("\nTop = " + stack.peek());
+        System.out.println("Popped = " + stack.pop());
+        System.out.println("New Top = " + stack.peek()); // Exception will occur, as Stack is empty
+
     }
 
 }
