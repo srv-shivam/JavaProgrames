071acc40cfac5340d6a9c841223a1e13fa0a40c0
srv-shivam <shivamsrivastava222001@gmail.com>
2021-11-04 01:27:08 +0530

    Implementation of methods(like, size(), sum(), max(), height()) of custom BinaryTree.

--- binaryTreePEP_CODING/BinaryTree.java
+++ binaryTreePEP_CODING/BinaryTree.java
@@ -4,10 +4,10 @@ import java.util.Stack;
 
 public class BinaryTree {
 
-    private Node root;
+    private static Node root;
 
     BinaryTree() {
-        this.root = null;
+        root = null;
     }
 
     private static class Node {
@@ -32,7 +32,7 @@ public class BinaryTree {
         }
     }
 
-    void constructBinaryTree(Integer[] nodes) {
+    static void constructBinaryTree(Integer[] nodes) {
 
         root = new Node(nodes[0]); // pointing first array element to root
         Pair pairNode = new Pair(root, 1);
@@ -70,7 +70,7 @@ public class BinaryTree {
         }
     }
 
-    void displayTree(Node node) {
+    static void displayTree(Node node) {
         if (node == null) {
             return;
         }
@@ -85,20 +85,78 @@ public class BinaryTree {
         displayTree(node.right);
     }
 
-    void inOder(Node root) {
-        if (root == null) return;
-        inOder(root.left);
-        System.out.print(root.data + " ");
-        inOder(root.right);
+    /**
+     * Calculating height (count of nodes) of Binary Tree
+     *
+     * @return totalSize
+     */
+    static int size(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftSize = size(node.left);
+        int rightSize = size(node.right);
+
+        return (leftSize + rightSize + 1);
+    }
+
+    /**
+     * Calculating sum of nodes of Binary Tree
+     *
+     * @return totalSum
+     */
+    static int sum(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftSum = sum(node.left);
+        int rightSum = sum(node.right);
+
+        return (leftSum + rightSum + node.data);
+    }
+
+    /**
+     * Calculating maximum node of Binary Tree
+     *
+     * @return maxNode
+     */
+    static int max(Node node) {
+        if (node == null) {
+            return Integer.MIN_VALUE;
+        }
+        int leftMax = max(node.left);
+        int rightMax = max(node.right);
+
+        return (Math.max(node.data, Math.max(leftMax, rightMax)));
+    }
+
+    /**
+     * Calculating height of the Binary Tree
+     *
+     * @return height
+     */
+    static int height(Node node) {
+        if (node == null) {
+            return -1;  // If height is calculated by edges
+            //return 0  // If height is calculated by nodes
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        return (Math.max(leftHeight, rightHeight) + 1);
     }
 
     public static void main(String[] args) throws Exception {
         // Integer array of nodes to create Binary Tree
         Integer[] nodes = {50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null, null};
 
-        BinaryTree binaryTree = new BinaryTree();
-        binaryTree.constructBinaryTree(nodes);
-//        binaryTree.inOder(binaryTree.root);
-        binaryTree.displayTree(binaryTree.root);
+        constructBinaryTree(nodes);
+        System.out.println("Binary Tree -> ");
+        displayTree(root);
+
+        System.out.println("\nSize of Binary Tree : " + size(root));
+        System.out.println("Sum of nodes of Binary Tree : " + sum(root));
+        System.out.println("Maximum node of Binary Tree : " + max(root));
+        System.out.println("Height of Binary Tree : " + height(root));
     }
 }
