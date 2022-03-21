0f8aaf2b04bd12838a19c4dc1ab8e31506b5d34a
srv-shivam <shivamsrivastava222001@gmail.com>
2022-03-21 13:57:01 +0530

    Program to find duplicate element in array of N+1 size with element ranging from[1,N] without using extra space.

---
+++ seriousPracticeDSA/arraysQuestions/Problem6.java
@@ -0,0 +1,43 @@
+package seriousPracticeDSA.arraysQuestions;
+
+/*
+ * Return the duplicate element in array of size N+1 with elements containing [1..N] without using extra space
+ *
+ * eg: I/P: [3,1,3,4,2]   Here, N=5, Elements range between [1..4]
+ *     O/P: 3
+ */
+import java.util.Scanner;
+
+public class Problem6 {
+
+    static int duplicateElement(int[] arr) {
+
+        for (int i=0; i<arr.length; ++i) {
+
+            int idx = Math.abs(arr[i]);
+            int value = arr[idx];
+
+            if (value < 0) {
+                return idx;
+            } else {
+                arr[idx] *= -1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i=0; i<n; ++i) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Duplicate element: " + duplicateElement(arr));
+    }
+
+}
