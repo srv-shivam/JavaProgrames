89633a7c60e9fef2b6bc9aec114731609f5cef41
srv-shivam <shivamsrivastava222001@gmail.com>
2022-03-24 15:03:14 +0530

    Program to get the sum of digits of number using recursion.

---
+++ seriousPracticeDSA/recursionQuestions/Problem2.java
@@ -0,0 +1,30 @@
+package seriousPracticeDSA.recursionQuestions;
+
+/*
+ * Program to get the sum of digits of a number
+ *
+ * eg: I/P: 123
+ *     O/P: 6 (1 + 2 + 3)
+ */
+
+import java.util.Scanner;
+public class Problem2 {
+
+    static int digitSum(int n, int sum) {
+
+        if (n < 10) {
+            return sum+n;
+        }
+        return digitSum(n/10, sum+(n%10));
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Sum of digits is : " + digitSum(n, 0));
+    }
+}
