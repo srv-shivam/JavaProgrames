3914e7536325b59ab051548cdf1d45fd07c97d2b
srv-shivam <shivamsrivastava222001@gmail.com>
2022-03-26 01:20:43 +0530

    Tower of Hanoi problem using recursion.

---
+++ seriousPracticeDSA/recursionQuestions/Problem5.java
@@ -0,0 +1,32 @@
+package seriousPracticeDSA.recursionQuestions;
+
+/*
+ * Program to write Tower of Hanoi Problem
+ */
+
+import java.util.Scanner;
+
+public class Problem5 {
+
+    static void generateMoves(int discs, char start, char auxiliary, char destination) {
+
+        if (discs == 1) {
+            System.out.println("Move " + discs + " from " + start + " to " + destination);
+            return;
+        }
+
+        generateMoves(discs - 1, start, destination, auxiliary);
+        System.out.println("Move " + discs + " from " + start + " to " + destination);
+        generateMoves(discs - 1, auxiliary, start, destination);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of discs: ");
+        int discs = sc.nextInt();
+
+        generateMoves(discs, 'A', 'B', 'C');
+    }
+}
