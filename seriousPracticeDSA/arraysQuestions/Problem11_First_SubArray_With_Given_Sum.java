f96ae0040d7daaf4a5e71c35b194ead72b283419
srv-shivam <shivamsrivastava222001@gmail.com>
2022-04-01 20:02:25 +0530

    Program to find a sub array with given sum (Optimized solution O(N))

--- seriousPracticeDSA/arraysQuestions/Problem11_First_SubArray_With_Given_Sum.java
+++ seriousPracticeDSA/arraysQuestions/Problem11_First_SubArray_With_Given_Sum.java
@@ -12,12 +12,41 @@ import java.util.Scanner;
 
 public class Problem11_First_SubArray_With_Given_Sum {
 
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    static ArrayList<Integer> findSubArray1(int[] arr, int sum) {
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        int temp = sum;
+        int i = 0, j = 0;
+
+        while (i < arr.length && j < arr.length) {
+
+            if (arr[j] <= temp) {
+                temp -= arr[j];
+                ++j;
+            } else {
+                temp += arr[i];
+                ++i;
+            }
+
+            if (temp == 0) {
+                ans.add(i + 1);
+                ans.add(j);
+                return ans;
+            }
+        }
+        // if the sub-array not found with given sum
+        ans.add(-1);
+        return ans;
+    }
+
     // Time Complexity: O(N^2)
     // Space Complexity: O(1)
     static ArrayList<Integer> findSubArray(int[] arr, int sum) {
 
         ArrayList<Integer> ans = new ArrayList<>();
-        int temp = 0;
+        int temp;
         for (int i = 0; i < arr.length; ++i) {
             temp = sum;
             for (int j = i; j < arr.length; ++j) {
@@ -54,6 +83,7 @@ public class Problem11_First_SubArray_With_Given_Sum {
         int sum = sc.nextInt();
 
         System.out.println("SubArray is: " + findSubArray(arr, sum));
+        System.out.println("SubArray is: " + findSubArray1(arr, sum));
 
     }
 
