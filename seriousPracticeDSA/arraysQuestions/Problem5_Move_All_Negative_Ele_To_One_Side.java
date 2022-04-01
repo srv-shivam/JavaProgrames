be7ebc859da37b13f7a15328f106e364f36bed09
srv-shivam <shivamsrivastava222001@gmail.com>
2022-04-01 22:56:55 +0530

    Program to find a sub array with given sum (Optimized solution O(N))

--- seriousPracticeDSA/arraysQuestions/Problem5_Move_All_Negative_Ele_To_One_Side.java
+++ seriousPracticeDSA/arraysQuestions/Problem5_Move_All_Negative_Ele_To_One_Side.java
@@ -6,14 +6,48 @@ package seriousPracticeDSA.arraysQuestions;
  * eg: I/P: 2 -12 8 3 -1 -4 5
  *     O/P: -12 -1 -4 2 8 3 5 (NOTE: Order of element does not matter)
  */
+
 import java.util.Scanner;
 
 public class Problem5_Move_All_Negative_Ele_To_One_Side {
 
+
+    // If we care about the sequence of all the elements whether it is positive or negative element
+    // this is the logic to implement such case
+    //
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    static void move(int[] arr) {
+
+        int count = 0;
+        int j = 0;
+        int[] neg = new int[arr.length];
+        for (int i = 0; i < arr.length; ++i) {
+
+            // negative element found
+            if (arr[i] < 0) {
+                neg[j++] = arr[i];
+            }
+            if (arr[i] > -1) {
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count++] = temp;
+            }
+        }
+
+        j += count;
+        for (int i = count, k = 0; i < j; ++i, ++k) {
+            arr[i] = neg[k];
+        }
+    }
+
+    // This is the logic when we only care about the positive elements sequences
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
     static void arrange(int[] arr, int n) {
 
-        int i=0, j=n-1;
-        while(i<j) {
+        int i = 0, j = n - 1;
+        while (i < j) {
             if (arr[i] < 0) ++i;
             if (arr[j] >= 0) ++j;
 
@@ -24,7 +58,8 @@ public class Problem5_Move_All_Negative_Ele_To_One_Side {
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
-                ++i; --j;
+                ++i;
+                --j;
             }
         }
     }
@@ -39,11 +74,12 @@ public class Problem5_Move_All_Negative_Ele_To_One_Side {
         System.out.print("Enter elements: ");
         int[] arr = new int[n];
 
-        for (int i=0; i<n; ++i) {
+        for (int i = 0; i < n; ++i) {
             arr[i] = sc.nextInt();
         }
 
-        arrange(arr, n);
+//        arrange(arr, n);
+        move(arr);
 
         System.out.print("Arranged array: ");
         for (int val : arr) System.out.print(val + " ");
