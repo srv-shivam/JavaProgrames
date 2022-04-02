066f226272ad28aab6268e3b5b5feea6b1737193
srv-shivam <shivamsrivastava222001@gmail.com>
2022-04-02 21:00:39 +0530

    Program to generate PERMUTATION OF STRING/ARRAY with OPTIMIZED method.

--- seriousPracticeDSA/recursionQuestions/Problem14_String_Permutation.java
+++ seriousPracticeDSA/recursionQuestions/Problem14_String_Permutation.java
@@ -11,6 +11,32 @@ import java.util.Scanner;
 
 public class Problem14_String_Permutation {
 
+    static void swap(StringBuilder str, int i, int j) {
+        char ch = str.charAt(i);
+        str.setCharAt(i, str.charAt(j));
+        str.setCharAt(j, ch);
+    }
+
+    //LOGIC:  Basic logic behind is we are just swapping each element accordingly, we are giving
+    //        each character chance to start with first position and generating permutation accordingly
+    //
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    static void generatePermutations2(int idx, StringBuilder str) {
+
+        if (idx == str.length()) {
+            System.out.println(str);
+            return;
+        }
+
+        for (int i = idx; i < str.length(); ++i) {
+            swap(str, i, idx);
+            generatePermutations2(idx + 1, str);
+            swap(str, i, idx);
+        }
+    }
+
+
     // Time Complexity: O(N! * N)
     // Space Complexity: O(N) + O(N) {Depth of recursion + Auxiliary space flag}
     //
@@ -42,5 +68,8 @@ public class Problem14_String_Permutation {
         boolean[] flag = new boolean[str.length()];
 
         generatePermutations1(str, new StringBuilder(""), flag);
+
+        System.out.println("\nUsing Second optimized method ");
+        generatePermutations2(0, new StringBuilder(str));
     }
 }
