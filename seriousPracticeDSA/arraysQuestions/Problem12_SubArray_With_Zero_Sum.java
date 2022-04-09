538521d89edae07090d5d9f9661e29941e94eb7a
srv-shivam <shivamsrivastava222001@gmail.com>
2022-04-09 23:58:30 +0530

    Program to check if a sub-array with zero-sum is present or not.

---
+++ seriousPracticeDSA/arraysQuestions/Problem12_SubArray_With_Zero_Sum.java
@@ -0,0 +1,37 @@
+package seriousPracticeDSA.arraysQuestions;
+
+import java.util.Scanner;
+
+public class Problem12_SubArray_With_Zero_Sum {
+
+    static boolean isSubArray(int[] arr) {
+
+        for (int i = 0; i < arr.length; ++i) {
+            int sum = arr[i];
+            for (int j = i + 1; j < arr.length; ++j) {
+                if (sum == 0) return true;
+                sum += arr[j];
+            }
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.print("Enter elements: ");
+        for (int i = 0; i < n; ++i) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(isSubArray(arr));
+    }
+
+
+}
