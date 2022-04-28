e97e15c639e009985123c3d69cac0adbece188d0
srv-shivam <shivamsrivastava222001@gmail.com>
2022-04-28 20:59:30 +0530

    Default Method in Java 8.

--- Java8/Test1.java
+++ Java8/Test1.java
@@ -2,7 +2,7 @@ package Java8;
 
 /**
  * Default Method | Virtual Extension Method | Defender Method
- *
+ * <p>
  * Without effecting implementation classes if you want to add a new method then we should go for ==> Default Method
  */
 
@@ -12,7 +12,33 @@ interface MyInterface2 {
     }
 }
 
-public class Test1 implements MyInterface2 {
+interface Left {
+    default void m1() {
+        System.out.println("Left Interface m1 method");
+    }
+}
+
+interface Right {
+    default void m1() {
+        System.out.println("Right Interface m1 method");
+    }
+}
+
+public class Test1 implements MyInterface2, Left, Right {
+
+    @Override
+    public void m1() {
+//        Left.super.m1(); ==> If wanted to use Left Interface implementation
+//        Right.super.m1();  ==> If wanted to use Right Interface implementation
+//        System.out.println("Our own m1 method implementation");  ==> If we want to give our own implementation
+    }
+
+    /**
+     * Two interfaces have same method name and both interfaces are implemented by Test1 class
+     * so to remove the unrelated method error we can override and give our own implementation
+     * or by using super we can use the definition of m1 method of respective parent interface
+     */
+
 
     public void display() {
         System.out.println("Overriding Implementation of Default Method");
@@ -21,6 +47,9 @@ public class Test1 implements MyInterface2 {
     public static void main(String[] args) {
         MyInterface2 myInterface2 = new Test1();
         myInterface2.display();
+
+        Test1 obj1 = new Test1();
+        obj1.m1();
     }
 
 }
