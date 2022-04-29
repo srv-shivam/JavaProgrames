3829bd491b041fac6e426d6bec6f190b028e6b1b
srv-shivam <shivamsrivastava222001@gmail.com>
2022-04-30 02:17:43 +0530

    Types of Predefined Functional Interfaces in Java 8

---
+++ Java8/Test2.java
@@ -0,0 +1,41 @@
+package Java8;
+
+/*
+ * package:- java.util.function;
+ * <p>
+ * <p>
+ * Predefined Functional Interface:
+ * 1) Predicate -> test method
+ * 2) Function
+ * 3) Consumer
+ * 4) Supplier
+ * ....
+ * <p>
+ * Two arguments Functional Interfaces:
+ * 1) BiPredicate
+ * 2) BiFunction
+ * 3) BiConsumer
+ * ....
+ * <p>
+ * Primitive Functional Interfaces:
+ * 1) IntPredicate
+ * 2) IntFunction
+ * 3) IntConsumer
+ * ....
+ */
+
+import java.util.function.*;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        // Use of Predicate Function Interface
+        Predicate<Integer> p = i -> i % 2 == 0;
+        System.out.println(p.test(10));
+        System.out.println(p.test(15));
+
+    }
+
+
+}
